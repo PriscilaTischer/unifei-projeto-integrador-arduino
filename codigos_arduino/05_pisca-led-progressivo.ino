d1db60c8c102e098abacde88140ed8f1dbbb6276
Priscila Tischer <pryscilaticher@hotmail.com>
2025-07-26 06:12:11 -0300

    Create 05_pisca-led-progressivo.ino
    Adiciona código de pisca-led com tempo crescente

---
+++ codigos_arduino/05_pisca-led-progressivo.ino
@@ -0,0 +1,14 @@
+int tempo;
+
+void setup() {
+  pinMode (13, OUTPUT);
+  tempo = 1000;
+}
+
+void loop() {
+  digitalWrite(13, HIGH);
+  delay(tempo);
+  digitalWrite(13, LOW);
+  delay(tempo);
+  tempo = tempo + 1000;
+}
